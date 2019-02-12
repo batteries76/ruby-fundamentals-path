eaea26974e1ffd6aaf539466e968f11fafe3c58f
Matt McKenzie <mattmckenzie@Matts-MacBook-Pro.local>
2019-02-12 16:37:23 +1100

    Changes to ordering and drills

---
+++ 1_ruby_drills_A.rb
@@ -0,0 +1,54 @@
+# Ruby Drills A
+
+# Sometimes you learn how to do things before you fully understand how they work. Coding is like this a lot. There are many things that I learned how to do well before I asked how they function. There are still plenty more things where that is the case for me. You merely push back that barrier slowly, but the unknown remains. In that vein, these are exercises that are designed to get you familiar with the lingo of arrays, hashes, and classes and objects, and get you repeatedly using them. It’s not a test, or even a challenge - it’s practice. Consider it like doing a forehand drill in tennis, or scales in music. You wouldn’t be upset making some mistakes while you practice, because you know that this is the thing you need to work toward being competent, and you allow yourself to fail and learn. This is exactly the same. 
+
+# Learn and fail as you need here, because the bigger tests are to come. As you get more comfortable with the basic syntax, and getting your code to work, you will have more brain space to think about what it is it all means. (And we are more than happy to keep explaining that at the same time too!)
+
+# Follow the instructions carefully. They will be repetitive and are not designed to be interesting, but rather build up ‘muscle memory’. *Each* line of code that you write, you *must* save the file and run the code. If there are *any* errors, you fix them then and there. If in doubt, puts out the structure you have made to make sure that it is what you hoped it was. I’m looking for accuracy - I want the instructions followed precisely. It will be your choice of class and variable names (etc), but the functioning and number of attributes called for are immutable. Ideally you’ll want the work to be checked, so copy and paste this text into a file and comment it out, so that I can check it easily - put the answer directly below the question.
+
+# 1. Define three different arrays that contain a minimum of three numbers.
+# 2. Access a value within each of these arrays using indexes. 
+# ———————————————————
+
+# 3. Define four arrays that contain a minimum of two strings.
+# 4. Access a value from each of these arrays using an index.
+# ———————————————————
+
+# 5. Define three arrays that contain floating point numbers. 
+# 6. Access a value from each of these arrays.
+# 7. Loop through each of the previous arrays, and print to screen the value of each member of the array.
+# ———————————————————
+
+# 8. Define four hashes that each have two keys. Make the value of the keys numbers.
+# 9. Access a value within each of these hashes.
+# ———————————————————
+
+# 10. Define three hashes that have three keys in each, and set the keys to floats.
+# 11. Access a value from each of these hashes.
+# ———————————————————
+
+# 12. Define two hashes that have three keys in each, and set the value of these keys to be strings.
+# ———————————————————
+
+# 13. Define two hashes that each have three keys, and set each of the keys as a different datatype, integer, float, and string.
+# 14. Define an array, and fill it with the hashes from the previous question.
+# 15. Loop through this array, printing to screen the value of each member of the array.
+# ———————————————————
+
+# 16. Define three arrays of numbers (minimum elements two).
+# 17. Define an array, and set each index of that array to be each of the arrays you defined in the previous question.
+# 18. Loop through the array that you have created, and print to screen the value of each element in the array.
+# ———————————————————
+
+# 19. Define three hashes, each with 3 keys, and give the keys the values of arrays (with a minimum of two elements in each). 
+# 20. Access one of these arrays within each of these hashes.
+# 21. Create three loops from each of the three hashes that you created above by accessing the arrays within.
+# ———————————————————
+
+# 22. Make a method that takes two arguments, and returns the result of them added together.
+# 23. Call the above method four times with different inputs each time. Make at least some of the inputs floats. 
+# 24. Define two integer and two float variables, and use them as arguments to this method you have made.
+# ———————————————————
+
+# 25. Make a hash with four keys, and have the keys be three different data types.
+# 26. Access two of the values in your hash, and print them to the screen.
---
+++ 4_ruby_drills_B.rb
@@ -0,0 +1,74 @@
+# Ruby Drills B
+
+# Sometimes you learn how to do things before you fully understand how they work. Coding is like this a lot. There are many things that I learned how to do well before I asked how they function. There are still plenty more things where that is the case for me. You merely push back that barrier slowly, but the unknown remains. In that vein, these are exercises that are designed to get you familiar with the lingo of arrays, hashes, and classes and objects, and get you repeatedly using them. It’s not a test, or even a challenge - it’s practice. Consider it like doing a forehand drill in tennis, or scales in music. You wouldn’t be upset making some mistakes while you practice, because you know that this is the thing you need to work toward being competent, and you allow yourself to fail and learn. This is exactly the same. 
+
+# Learn and fail as you need here, because the bigger tests are to come. As you get more comfortable with the basic syntax, and getting your code to work, you will have more brain space to think about what it is it all means. (And we are more than happy to keep explaining that at the same time too!)
+
+# Follow the instructions carefully. They will be repetitive and are not designed to be interesting, but rather build up ‘muscle memory’. *Each* line of code that you write, you *must* save the file and run the code. If there are *any* errors, you fix them then and there. If in doubt, puts out the structure you have made to make sure that it is what you hoped it was. I’m looking for accuracy - I want the instructions followed precisely. It will be your choice of class and variable names (etc), but the functioning and number of attributes called for are immutable. Ideally you’ll want the work to be checked, so copy and paste this text into a file and comment it out, so that I can check it easily - put the answer directly below the question.
+
+# 27. Define a class that has three attributes. Set one of those attributes at the time objects are initialised. Make one attribute readable, and the other two writeable.
+# 28. Make two instance methods in the class you defined that do something basic (anything you like).
+# 29. Make eight objects from the class you just made.
+# 30. Puts out an attribute of four of the objects (instances of the class) that you made.
+# 31. Assign three values to attributes of three of the objects you have made (those that have write access).
+# ———————————————————
+
+# 32. Create a hash with one key and value pair.
+# 33. Define a method that takes a hash as the input, and puts out the value of one of the keys of the hash. 
+# 34. Call the previous method with that hash as the argument
+# ———————————————————
+
+# 35. Make a hash with three keys and values, and assign it to a variable.
+# 36. Make four more hashes with the same keys.
+# 37. Access a value from three of these hashes.
+# 38. Define a variable that will be assigned an empty array.
+# 39. Define a method that takes an array and a hash as arguments. The function will push the hash into the array given.
+# 40. Using the method, the four hashes, and the array that you have made in the previous steps, call the method four times to put those four hashes into the array.
+# 41. Make sure that the array contains four hashes (maybe puts).
+# 42. Access two elements of the array that you have created (via an index).
+# 43. For one of these, now access one of the values of the hash at that is located at that index.
+# ———————————————————
+
+# 44. Make an class that has six attributes and three instance methods. Make two of the attributes readable, three writeable, and one both readable and writable. 
+# 45. Set two of the attributes when the objects are initialised, but do this by expecting one hash as the argument (the one argument will set two attribute values). 
+# 46. Make five objects from the class you made.
+# 47. Puts the value of three separate objects’ attributes.
+# 48. Call two of the instance methods on three of these objects.
+# ———————————————————
+
+# 49. Write a method that takes five arguments: three strings, and two numbers.
+# 50. Call that method twice.
+# ———————————————————
+
+# 51. Write a method that takes two arguments - one a hash, and the other an array. Inside the method puts out a value from the hash (assume that you will know the keys), and an element from the array.
+# 52. Define four hashes (with the previous method in mind), one with one key, one with two keys, one with three keys, and one with four.
+# 53. Define two arrays, with two different datatypes.
+# 54. Call the previous method four times, once with each hash, and use the arrays you made.
+# ———————————————————
+
+# 55. Make a class that has nine attributes. Three attributes are set when the objects are initialised, but the initialise method only takes one argument, a hash. 
+# 56. Write four instance methods for your class. 
+# 57. Make two of the attributes read accessible. Make two of them writable. And make three of them both readable and writable.
+# 58. Make seven objects from this class.
+# 59. Make four variables: a hash, an array, an integer, and boolean.
+# 60. Assign these variables to attributes from the various objects you made. 
+# 61. Puts to screen two attributes from four different objects.
+# 62. Call two instance methods on four different objects. 
+# 63. Write a method that takes five arguments: three strings, and two numbers.
+# 64. Call that method twice.
+# ———————————————————
+
+# 65. Write a method that takes two arguments - one a hash, and the other an array. Inside the method puts out a value from the hash, and an element from the array.
+# 66. Define four hashes, one with one key, one with two keys, one with three keys, and one with four.
+# 67. Define two arrays, with two different datatypes.
+# 68. Call the method four times, once with each hash, and use the arrays you made.
+# ———————————————————
+
+# 69. Make a class that has nine attributes. Three attributes are set when the objects are initialised, but the initialise method only takes one argument, a hash.
+# 70. Write four instance methods for your class.
+# 71. Make two of the attributes read accessible. Make two of them writable. And make three of them both readable and writable.
+# 72. Make seven objects from this class.
+# 73. Make four variables: a hash, an array, an integer, and boolean.
+# 74. Assign these variables to attributes from the various objects you made.
+# 75. Puts to screen two attributes from four different objects.
+# 76. Call two instance methods on four different objects.
