d3e3bd17d1d75ec91e519af5b4e9d66794329cc3
Matt McKenzie <mattmckenzie@Matts-MacBook-Pro.local>
2019-02-21 10:58:53 +1100

    formatting changes

--- 1_ruby_drills_A.rb
+++ 1_ruby_drills_A.rb
@@ -8,47 +8,47 @@
 
 # 1. Define three different arrays that contain a minimum of three numbers.
 # 2. Access a value within each of these arrays using indexes. 
-# ———————————————————
 
+# ———————————————————
 # 3. Define four arrays that contain a minimum of two strings.
 # 4. Access a value from each of these arrays using an index.
-# ———————————————————
 
+# ———————————————————
 # 5. Define three arrays that contain floating point numbers. 
 # 6. Access a value from each of these arrays.
 # 7. Loop through each of the previous arrays, and print to screen the value of each member of the array.
-# ———————————————————
 
+# ———————————————————
 # 8. Define four hashes that each have two keys. Make the value of the keys numbers.
 # 9. Access a value within each of these hashes.
-# ———————————————————
 
+# ———————————————————
 # 10. Define three hashes that have three keys in each, and set the keys to floats.
 # 11. Access a value from each of these hashes.
-# ———————————————————
 
+# ———————————————————
 # 12. Define two hashes that have three keys in each, and set the value of these keys to be strings.
-# ———————————————————
 
+# ———————————————————
 # 13. Define two hashes that each have three keys, and set each of the keys as a different datatype, integer, float, and string.
 # 14. Define an array, and fill it with the hashes from the previous question.
 # 15. Loop through this array, printing to screen the value of each member of the array.
-# ———————————————————
 
+# ———————————————————
 # 16. Define three arrays of numbers (minimum elements two).
 # 17. Define an array, and set each index of that array to be each of the arrays you defined in the previous question.
 # 18. Loop through the array that you have created, and print to screen the value of each element in the array.
-# ———————————————————
 
+# ———————————————————
 # 19. Define three hashes, each with 3 keys, and give the keys the values of arrays (with a minimum of two elements in each). 
 # 20. Access one of these arrays within each of these hashes.
 # 21. Create three loops from each of the three hashes that you created above by accessing the arrays within.
-# ———————————————————
 
+# ———————————————————
 # 22. Make a method that takes two arguments, and returns the result of them added together.
 # 23. Call the above method four times with different inputs each time. Make at least some of the inputs floats. 
 # 24. Define two integer and two float variables, and use them as arguments to this method you have made.
-# ———————————————————
 
+# ———————————————————
 # 25. Make a hash with four keys, and have the keys be three different data types.
 # 26. Access two of the values in your hash, and print them to the screen.
